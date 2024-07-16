6f44146b6bdac3684499a94cf4ebe5984e360a45
Nischal lamichhane <nischallc56@gmail.com>
2024-07-16 16:36:58 +0545

    (new):adding signup functionality

--- Mailing/settings.py
+++ Mailing/settings.py
@@ -6,6 +6,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
+print(DEBUG)
 
 ALLOWED_HOSTS = config('ALLOWED_HOSTS').split(' ')
 
--- home/urls.py
+++ home/urls.py
@@ -2,10 +2,11 @@ from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views 
+from . import utils as util
 
 urlpatterns = [
     path('', views.home, name='index'),
     path('base/', views.base, name='base'),
     path('create/mailing/', views.create_mailing, name='create_mailing'),
-    path('confirm-mailing/<str:token>/', views.confirm_mailing, name='confirm_mailing'),
+    path('confirm-mailing/<str:token>/', util.confirm_mailing, name='confirm_mailing'),
 ] 
