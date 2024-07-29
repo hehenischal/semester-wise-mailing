985e7405e1fb45e74b44996e22420b14fdc6f8ae
Nischal lamichhane <nischallc56@gmail.com>
2024-07-30 05:21:54 +0545

    (new):adding analytics in admin panel

--- tracking/views.py
+++ tracking/views.py
@@ -9,9 +9,9 @@ def track(request, token):
     mail_seen = get_object_or_404(MailSeen, token=token)
     seen_mails = request.COOKIES.get('seen_mails', '')
 
-    if str(mail_seen.mailing.id) not in seen_mails.split(','):
+    if str(mail_seen.mailing_id) not in seen_mails.split(','):
         mail_seen.increment_seen_count()
-        seen_mails = f"{seen_mails},{mail_seen.mailing.id}" if seen_mails else str(mail_seen.mailing.id)
+        seen_mails = f"{seen_mails},{mail_seen.mailing_id}" if seen_mails else str(mail_seen.mailing_id)
 
     response = FileResponse(open(Path(settings.STATIC_ROOT) / '1x1.png', 'rb'), content_type='image/png')
     response.set_cookie('seen_mails', seen_mails, max_age=365*24*60*60)  # Set cookie for 1 year
